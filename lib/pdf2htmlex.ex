ce71b4aa88ba84a2b087ebdc497b4ffa914d6440
Richard Nyström <ricn@rny.io>
2015-07-03 13:28:32 +0200

    Refactoring

--- lib/pdf2htmlex.ex
+++ lib/pdf2htmlex.ex
@@ -1,19 +1,11 @@
 defmodule Pdf2htmlex do
-
-  def convert(opts) when is_list(opts) do
-    exec_cmd opts
-  end
-
-  def save_to(opts, dir) when is_list(opts) do
-    ["--dest-dir", dir] ++ opts
-  end
+  def open(pdf) when is_binary(pdf), do: [pdf]
+  def save_to(opts, dir) when is_list(opts), do: ["--dest-dir", dir] ++ opts
+  def convert(opts) when is_list(opts), do: exec_cmd opts
 
   def zoom(opts, zoom_ratio) when is_list(opts) do
-    ["--zoom", Float.to_string(zoom_ratio, [decimals: 1, compact: true])] ++ opts
-  end
-
-  def open(pdf) when is_binary(pdf) do
-    [pdf]
+    zoom_as_str = Float.to_string(zoom_ratio, [decimals: 1, compact: true])
+    ["--zoom", zoom_as_str] ++ opts
   end
 
   defp exec_cmd(opts) do
