6540c5d4e69ea6b9bfb3c1c2203624887ef8329f
Richard Nyström <ricn@rny.io>
2015-07-03 15:11:05 +0200

    Cleaned up the code

--- lib/pdf2htmlex.ex
+++ lib/pdf2htmlex.ex
@@ -3,33 +3,17 @@ defmodule Pdf2htmlex do
   def save_to(opts, dir) when is_list(opts), do: ["--dest-dir", dir] ++ opts
   def convert!(opts) when is_list(opts), do: exec_cmd opts
 
-  def zoom(opts, zoom_ratio) when is_list(opts) do
-    zoom_as_str = Float.to_string(zoom_ratio, [decimals: 1, compact: true])
-    ["--zoom", zoom_as_str] ++ opts
-  end
-
-  def first_page(opts, first_p) when is_list(opts) do
-    first_p_as_str = Integer.to_string(first_p)
-    ["--first-page", first_p_as_str] ++ opts
-  end
-
-  def last_page(opts, last_p) when is_list(opts) do
-    last_p_as_str = Integer.to_string(last_p)
-    ["--last-page", last_p_as_str] ++ opts
-  end
-
-  def fit_width(opts, width) when is_list(opts) do
-    width_as_str = Integer.to_string(width)
-    ["--fit-width", width_as_str] ++ opts
-  end
-
-  def fit_height(opts, height) when is_list(opts) do
-    height_as_str = Integer.to_string(height)
-    ["--fit-height", height_as_str] ++ opts
-  end
+  def zoom(opts, zoom_ratio) when is_list(opts) and is_float(zoom_ratio), do: ["--zoom", f_to_s(zoom_ratio)] ++ opts
+  def first_page(opts, first) when is_list(opts) and is_integer(first), do: ["--first-page", i_to_s(first)] ++ opts
+  def last_page(opts, last) when is_list(opts) and is_integer(last), do: ["--last-page", i_to_s(last)] ++ opts
+  def fit_width(opts, width) when is_list(opts) and is_integer(width), do: ["--fit-width", i_to_s(width)] ++ opts
+  def fit_height(opts, height) when is_list(opts) and is_integer(height), do: ["--fit-height", i_to_s(height)] ++ opts
 
   defp exec_cmd(opts) do
     cmd = System.find_executable("pdf2htmlex")
     System.cmd(cmd, opts, stderr_to_stdout: true)
   end
+
+  defp i_to_s(s), do: Integer.to_string(s)
+  defp f_to_s(f), do: Float.to_string(f, [decimals: 1, compact: true])
 end
