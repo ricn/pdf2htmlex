76b1071af9eba9267f7b07fdeda3e069f473badb
Richard Nyström <ricn@rny.io>
2015-07-05 00:19:21 +0200

    Prepared the mix file for publishing

--- mix.exs
+++ mix.exs
@@ -2,21 +2,42 @@ defmodule Pdf2htmlex.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :pdf2htmlex,
+    [
+     app: :pdf2htmlex,
      version: "0.1.0",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     description: description,
+     package: package
+     deps: deps
+    ]
   end
 
   def application do
     [applications: [:logger]]
   end
 
+  defp description do
+    """
+    Convert PDF docs to beautiful HTML files without losing text or format.
+    """
+  end
+
   defp deps do
-    [{:secure_random, "~> 0.1"},
-     {:earmark, "~> 0.1", only: :dev},
-     {:ex_doc, "~> 0.7", only: :dev}]
+    [
+      {:secure_random, "~> 0.1"},
+      {:earmark, "~> 0.1", only: :dev},
+      {:ex_doc, "~> 0.7", only: :dev}
+    ]
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      contributors: ["Richard Nyström"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/ricn/pdf2htmlex", "Docs" => "http://hexdocs.pm/pdf2htmlex"}
+    ]
   end
 end
