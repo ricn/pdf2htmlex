407cc24d357d7b7d193557a6fc6fdfe44154e06d
Richard Nyström <ricn@rny.io>
2015-07-04 10:43:32 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Pdf2htmlex.Mixfile do
 
   def project do
     [app: :pdf2htmlex,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
