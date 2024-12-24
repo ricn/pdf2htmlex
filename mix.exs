b9c4f21cb25935cb4ebfbd3dab32e65c3f07752c
Akaash Choudhary <akaash@sericin.net>
2024-12-24 10:12:15 +0500

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -8,9 +8,9 @@ defmodule Pdf2htmlex.Mixfile do
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     description: description,
-     package: package,
-     deps: deps
+     description: description(),
+     package: package(),
+     deps: deps()
     ]
   end
 
